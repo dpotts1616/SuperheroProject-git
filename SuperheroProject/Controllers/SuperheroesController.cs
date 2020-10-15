d30431b43110ce8ccb80c3f518fe763a0fed758d
unknown <dylan.potts16@gmail.com>
2020-10-15 15:53:14 -0400

    initial upload

---
+++ SuperheroProject/Controllers/SuperheroesController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperheroProject.Data;
+using SuperheroProject.Models;
+
+namespace SuperheroProject.Controllers
+{
+    public class SuperheroesController : Controller
+    {
+        private ApplicationDbContext db;
+        public SuperheroesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+
+        // GET: SuperheroesController
+        public ActionResult Index()
+        {
+            
+            return View(db);
+        }
+
+        // GET: SuperheroesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperheroesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                //TODO: Add insert logic here
+                db.Superheroes.Add(superhero);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SuperheroProject/Models/Superhero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroProject.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Superhero Name")]
+        public string SuperName { get; set; }
+        [Display(Name = "Alter Ego First Name")]
+        public string AlterEgoFirst { get; set; }
+        [Display(Name = "Alter Ego Last Name")]
+        public string AlterEgoLast { get; set; }
+        [Display(Name = "Primary Ability")]
+        public string PrimaryAbility { get; set; }
+        [Display(Name = "Secondary Ability")]
+        public string SecondaryAbility { get; set; }
+        [Display(Name = "Catchphrase")]
+        public string Catchphrase { get; set; }
+
+        public Superhero()
+        {
+
+        }
+    }
+}
